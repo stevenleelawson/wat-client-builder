2a2de4baf263b857d9532bb84a926e6bb8564cbe
Steven Lawson <unluckygamblermusic@gmail.com>
2016-08-18 12:54:51 -0600

    got the image right, but not the font

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ class App extends Component {
       <nav className="navbar navbar-default">
     <div className="container-fluid">
       <div className="navbar-header">
-        <a className="navbar-brand" href="/"><img src="../images/updatedlogo.png" alt="logo"  /></a>
+        <a className="navbar-brand" href="/"><img className="logo" src="../images/updatedlogo.png" alt="logo"  /></a>
       </div>
 
       <div className="collapse navbar-collapse headline" id="bs-example-navbar-collapse-1">
-        <h1>W.A.T. <span>WE ARE TOOLS</span></h1>
+        <h1 className="textStuff">W.A.T. <span>WE ARE TOOLS</span></h1>
         <h2><em>-Tools for learning web development</em></h2>
       </div>
     </div>
