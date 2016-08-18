156e085957539057e43c25e473f008ef6e0179bc
Andy Knolla <andyknolla@gmail.com>
2016-08-18 11:38:18 -0600

    add header

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import Card from './Card';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Card />
-      </div>
+      <nav className="navbar navbar-default">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <a className="navbar-brand" href="/"><img src="../images/updatedlogo.png" alt="logo"  /></a>
+      </div>
+
+      <div className="collapse navbar-collapse headline" id="bs-example-navbar-collapse-1">
+        <h1>W.A.T. <span>WE ARE TOOLS</span></h1>
+        <h2><em>-Tools for learning web development</em></h2>
+      </div>
+    </div>
+  </nav>
     );
   }
 }
